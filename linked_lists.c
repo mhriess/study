d38639728cb56a0cb92dea985e19c3d87d97c281
Michael Riess <mike@lumity.com>
2017-02-16 15:20:28 -0500

    Stanford Linked Lists Problems 8 through 13

--- linked_lists.c
+++ linked_lists.c
@@ -226,6 +226,148 @@ void InsertSort(struct node** headRef) {
   *headRef = newHead;
 }
 
+// Append 'b' onto the end of 'a', and then set 'b' to NULL.
+void Append(struct node** aRef, struct node** bRef) {
+  if (*aRef == NULL) {
+    *aRef = *bRef;
+  } else {
+    struct node* a = *aRef;
+    struct node* b = *bRef;
+
+    while (a->next != NULL) {
+      a = a->next;
+    }
+
+    while (b != NULL) {
+      Push(&(a->next), b->data);
+      a = a->next;
+      b = b->next;
+    }
+  }
+
+  *bRef = NULL;
+}
+
+/*
+ Split the nodes of the given list into front and back halves,
+ and return the two lists using the reference parameters.
+ If the length is odd, the extra node should go in the front list.
+*/
+void FrontBackSplit(struct node* source, struct node** frontRef, struct node** backRef) {
+  int length = Length(source);
+  struct node* current = source;
+
+  if (length < 2) {
+    *frontRef = source;
+    *backRef = NULL;
+  } else {
+    int split = (length - 1) / 2;
+
+    for (int i = 0; i < split; i++) {
+      current = current->next;
+    }
+
+    *frontRef = source;
+    *backRef = current->next;
+    current->next = NULL;
+  }
+}
+
+// Remove duplicates from a sorted list.
+void RemoveDuplicates(struct node* head) {
+  struct node* current = head;
+
+  while (current != NULL) {
+    struct node* next = current->next;
+
+    while (next != NULL && next->data == current->data) {
+      struct node* nextNext = next->next;
+      free(next);
+      next = nextNext;
+    }
+
+    current->next = next;
+    current = current->next;
+  }
+}
+
+/*
+ Take the node from the front of the source, and move it to
+ the front of the dest.
+ It is an error to call this with the source list empty.
+*/
+void MoveNode(struct node** destRef, struct node** sourceRef) {
+  struct node* head = *destRef;
+  struct node* target = *sourceRef;
+
+  *sourceRef = target->next;
+  target->next = head;
+  *destRef = target;
+}
+
+/*
+ Given the source list, split its nodes into two shorter lists.
+ If we number the elements 0, 1, 2, ... then all the even elements
+ should go in the first list, and all the odd elements in the second.
+ The elements in the new lists may be in any order.
+*/
+void AlternatingSplit(struct node* source, struct node** aRef, struct node** bRef) {
+  struct node* current = source;
+  int index = 0;
+
+  while (current != NULL) {
+    struct node* next = current->next;
+
+    if (index % 2 == 0) {
+      MoveNode(aRef, &current);
+    } else {
+      MoveNode(bRef, &current);
+    }
+
+    index++;
+    current = next;
+  }
+}
+
+/*
+ Merge the nodes of the two lists into a single list taking a node
+ alternately from each list, and return the new list.
+*/
+struct node* ShuffleMerge(struct node* a, struct node* b) {
+  struct node* merged = NULL;
+  struct node* headA = a;
+  struct node* headB = b;
+  struct node* last = merged;
+
+  while (headA != NULL && headB != NULL) {
+    if (merged == NULL) {
+      merged = headA;
+    } else {
+      last->next = headA;
+    }
+
+    struct node* nextA = headA->next;
+    struct node* nextB = headB->next;
+
+    headA->next = headB;
+    headB->next = NULL;
+    last = headB;
+
+    if (nextA == NULL) {
+      last->next = nextB;
+      nextB = NULL;
+    } else if (nextB == NULL) {
+      last->next = nextA;
+      nextA = NULL;
+    }
+
+    headA = nextA;
+    headB = nextB;
+  }
+
+  return merged;
+}
+
 /*
  Tests
 */
@@ -377,6 +519,167 @@ void InsertSortTest() {
   );
 }
 
+void AppendTest() {
+  Context("Problem 8: Append()");
+
+  struct node* listOne = NULL;
+  struct node* listTwo = BuildOneTwoThree();
+
+  Append(&listOne, &listTwo);
+  It("should set the first list to the second if the first list is empty",
+    Length(listOne) == 3 &&
+    GetNth(listOne, 1) == 2
+  );
+
+  struct node* listThree = BuildOneTwoThree();
+  struct node* listFour = BuildOneTwoThree();
+
+  Append(&listThree, &listFour);
+  It("should append the second list to the first",
+    Length(listThree) == 6 &&
+    GetNth(listThree, 4) == 2
+  );
+  It("should set the second list to null", listFour == NULL);
+}
+
+void FrontBackSplitTest() {
+  Context("Problem 9: FrontBackSplit()");
+
+  struct node* emptyList = NULL;
+  struct node* front = NULL;
+  struct node* back = NULL;
+
+  FrontBackSplit(emptyList, &front, &back);
+  It("should keep all lists as NULL", emptyList == NULL && front == NULL && back == NULL);
+
+  struct node* listTwo = NULL;
+  struct node* frontTwo = NULL;
+  struct node* backTwo = NULL;
+
+  Push(&listTwo, 4);
+  Push(&listTwo, 3);
+
+  FrontBackSplit(listTwo, &frontTwo, &backTwo);
+  It("should split a length two list into two equal lists",
+    Length(frontTwo) == 1 && GetNth(frontTwo, 0) == 3 &&
+    Length(backTwo) == 1 && GetNth(backTwo, 0) == 4
+  );
+
+  struct node* listThree = NULL;
+  struct node* frontThree = NULL;
+  struct node* backThree = NULL;
+
+  Push(&listThree, 4);
+  Push(&listThree, 3);
+  Push(&listThree, 2);
+
+  FrontBackSplit(listThree, &frontThree, &backThree);
+  It("should split a length three list into the first two elements and the last",
+    Length(frontThree) == 2 && GetNth(frontThree, 0) == 2 && GetNth(frontThree, 1) == 3 &&
+    Length(backThree) == 1 && GetNth(backThree, 0) == 4
+  );
+
+
+  struct node* listFour = NULL;
+  struct node* frontFour = NULL;
+  struct node* backFour = NULL;
+
+  Push(&listFour, 4);
+  Push(&listFour, 3);
+  Push(&listFour, 2);
+  Push(&listFour, 1);
+
+  FrontBackSplit(listFour, &frontFour, &backFour);
+  It("should split a length four list into the first two elements and the last two elements",
+    Length(frontFour) == 2 && GetNth(frontFour, 0) == 1 && GetNth(frontFour, 1) == 2 &&
+    Length(backFour) == 2 && GetNth(backFour, 0) == 3 && GetNth(backFour, 1) == 4
+  );
+}
+
+void RemoveDuplicatesTest() {
+  Context("Problem 10: RemoveDuplicates()");
+
+  struct node* list = BuildOneTwoThree();
+  Push(&list, 1);
+
+  RemoveDuplicates(list);
+  It("should remove duplicates from the beginning of the list",
+    Length(list) == 3 && GetNth(list, 0) == 1 && GetNth(list, 1) == 2
+  );
+
+  InsertNth(&list, 1, 2);
+  RemoveDuplicates(list);
+  It("should remove duplicates from the middle of the list",
+    Length(list) == 3 && GetNth(list, 1) == 2 && GetNth(list, 2) == 3
+  );
+
+  InsertNth(&list, 2, 3);
+  RemoveDuplicates(list);
+  It("should remove duplicates from the end of the list",
+    Length(list) == 3 && GetNth(list, 1) == 2 && GetNth(list, 2) == 3
+  );
+}
+
+void MoveNodeTest() {
+  Context("Problem 11: MoveNode()");
+
+  struct node* a = BuildOneTwoThree(); // the list {1, 2, 3}
+  struct node* b = BuildOneTwoThree();
+
+  MoveNode(&a, &b);
+  It("should move the head node of the second list to the front of the first",
+    Length(a) == 4 &&
+    Length(b) == 2 &&
+    GetNth(a, 0) == 1 &&
+    GetNth(a, 1) == 1 &&
+    GetNth(b, 0) == 2
+  );
+}
+
+void AlternatingSplitTest() {
+  Context("Problem 12: AlternatingSplit()");
+
+  struct node* list = BuildOneTwoThree();
+  struct node* a = NULL;
+  struct node* b = NULL;
+
+  AlternatingSplit(list, &a, &b);
+  It("should split the list in two",
+    Length(a) == 2 && GetNth(a, 0) == 3 && GetNth(a, 1) == 1 &&
+    Length(b) == 1 && GetNth(b, 0) == 2
+  );
+}
+
+void ShuffleMergeTest() {
+  Context("Problem 13: ShuffleMerge()");
+
+  struct node* a = BuildOneTwoThree();
+  struct node* b = BuildOneTwoThree();
+
+  struct node* merged = ShuffleMerge(a, b);
+  It("should alternately merge the nodes",
+    Length(merged) == 6 &&
+    GetNth(merged, 0) == 1 &&
+    GetNth(merged, 1) == 1 &&
+    GetNth(merged, 2) == 2 &&
+    GetNth(merged, 3) == 2
+  );
+
+  struct node* c = BuildOneTwoThree();
+  struct node* d = NULL;
+
+  Push(&d, 1);
+
+  struct node* merged2 = ShuffleMerge(c, d);
+  It("should alternately merge the nodes",
+    Length(merged2) == 4 &&
+    GetNth(merged2, 0) == 1 &&
+    GetNth(merged2, 1) == 1 &&
+    GetNth(merged2, 2) == 2 &&
+    GetNth(merged2, 3) == 3
+  );
+}
+
 int main(void) {
 
   PushTest();
@@ -389,6 +692,12 @@ int main(void) {
   InsertNthTest();
   SortedInsertTest();
   InsertSortTest();
+  AppendTest();
+  FrontBackSplitTest();
+  RemoveDuplicatesTest();
+  MoveNodeTest();
+  AlternatingSplitTest();
+  ShuffleMergeTest();
 
   return 0;
 }
